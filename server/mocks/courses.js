ab4750dc168250903fe2beb101d69f43dcd1a45b
Renaud Martinet <renaud.martinet@horoquartz.fr>
2015-04-01 13:42:33 +0200

    Use tee-card component in the round-card component

--- server/mocks/courses.js
+++ server/mocks/courses.js
@@ -18,7 +18,7 @@ module.exports = function(app) {
         id: req.params.id,
         name: 'Niort Hippodrome',
         holes: [1, 3, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18],
-        tees: [1]
+        tees: [1, 2, 3, 4, 5]
       }
     });
   });
--- server/mocks/tees.js
+++ server/mocks/tees.js
@@ -16,9 +16,53 @@ module.exports = function(app) {
     res.send({
       'tees': {
         id: 1,
+        color: 'red',
+        sss: 65.7,
+        slope: 118
+      }
+    });
+  });
+
+  teesRouter.get('/2', function(req, res) {
+    res.send({
+      'tees': {
+        id: 2,
+        color: 'blue',
+        sss: 66.8,
+        slope: 121
+      }
+    });
+  });
+
+  teesRouter.get('/3', function(req, res) {
+    res.send({
+      'tees': {
+        id: 3,
         color: 'yellow',
-        sss: 69.5,
-        slope: 126
+        sss: 69.7,
+        slope: 127
+      }
+    });
+  });
+
+  teesRouter.get('/4', function(req, res) {
+    res.send({
+      'tees': {
+        id: 4,
+        color: 'white',
+        sss: 71.7,
+        slope: 130
+      }
+    });
+  });
+
+  teesRouter.get('/5', function(req, res) {
+    res.send({
+      'tees': {
+        id: 5,
+        color: 'black',
+        sss: 71.7,
+        slope: 130
       }
     });
   });
