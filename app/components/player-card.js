ac24116883428fd98426d7b3caa06bd564c109ad
Renaud Martinet <renaud.martinet@horoquartz.fr>
2015-04-01 11:24:30 +0200

    Style the player component

--- app/components/player-card.js
+++ app/components/player-card.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  picture: function() {
+    return '/assets/images/default-user-pic.png';
+  }.property()
 });
--- app/styleguide/controller.js
+++ app/styleguide/controller.js
@@ -3,5 +3,8 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   tee: function() {
     return this.store.find('tee', 1);
+  }.property(),
+  player: function() {
+    return this.store.find('player', 1);
   }.property()
 });
