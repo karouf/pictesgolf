ab60070adc518839a93d884632b7b0a26f0f72f6
Renaud Martinet <renaud.martinet@horoquartz.fr>
2015-04-01 10:38:34 +0200

    Add some style to the styleguide

--- app/components/styleguide-html.js
+++ app/components/styleguide-html.js
@@ -3,8 +3,8 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   html: '',
 updateHtml: function() {
-  this.set('html', this.$().children()[2].innerHTML);
-  this.$().children()[2].hidden = true;
+  this.set('html', this.$().children()[0].innerHTML);
+  this.$().children()[0].hidden = true;
 },
 didInsertElement: function() {
   Ember.run.scheduleOnce('afterRender', this, this.updateHtml);
