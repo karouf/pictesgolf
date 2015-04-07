015b4139ef29939e95d031f68b9d8fbb90983e4f
Renaud Martinet <renaud.martinet@horoquartz.fr>
2015-04-07 10:10:41 +0200

    Display colors beneath scores relative to par

--- app/components/player-card.js
+++ app/components/player-card.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: 'td',
   picture: function() {
     return '/assets/images/default-user-pic.png';
   }.property()
---
+++ app/components/score-box.js
@@ -0,0 +1,49 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  tagName: 'td',
+  classNames: ['score-card__score'],
+  classNameBindings: ['toPar'],
+  toPar: function() {
+    var par = this.get('score.hole.par');
+    var strokes = this.get('score.strokes');
+
+    if(!par) { return null; }
+
+    var prefix = 'score-card__score--';
+    var perf = '';
+    var toPar =  strokes - par;
+    if(toPar > 3) { toPar = 3; }
+
+    switch(toPar) {
+      case -2:
+        perf = 'condor';
+        break;
+      case -2:
+        perf = 'albatross';
+        break;
+      case -2:
+        perf = 'eagle';
+        break;
+      case -1:
+        perf = 'birdie';
+        break;
+      case 0:
+        perf = 'par';
+        break;
+      case 1:
+        perf = 'bogey';
+        break;
+      case 2:
+        perf = 'double-bogey';
+        break;
+      case 3:
+        perf = 'triple-bogey';
+        break;
+      default:
+        perf = 'triple-bogey';
+    }
+
+    return prefix + perf;
+  }.property('score.strokes', 'score.hole.par')
+});
