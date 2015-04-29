836ea46c187e595c89ef0c11182fa674946a1783
Renaud Martinet <karouf@gmail.com>
2015-04-29 13:16:27 +0200

    POST /rounds error handling specs

--- spec/api/rounds_spec.rb
+++ spec/api/rounds_spec.rb
@@ -16,17 +16,76 @@ describe API do
   end
 
   describe "POST /rounds" do
-    before do
-      FactoryGirl.create(:course)
-      post '/rounds', round: { date: '2015-04-01', course_id: 1, holes_played: 9, scoring: 'stableford', competition: false }
+    describe 'with valid params' do
+      before do
+        FactoryGirl.create(:course)
+        post '/rounds', round: { date: '2015-04-01', course_id: 1, holes_played: 9, scoring: 'stableford', competition: false }
+      end
+
+      it 'responds with HTTP 201' do
+        last_response.must_be :created?
+      end
+
+      it 'returns the id of the created round' do
+        json['rounds']['id'].must_equal 1
+      end
     end
 
-    it 'responds with HTTP 201' do
-      last_response.must_be :created?
+    describe 'with no params' do
+      before do
+        post '/rounds'
+      end
+
+      it 'responds with HTTP 400' do
+        last_response.must_be :bad_request?
+      end
     end
 
-    it 'returns the id of the created round' do
-      json['rounds']['id'].must_equal 1
+    [
+      'round',
+      'round:date',
+      'round:course_id',
+      'round:holes_played',
+      'round:scoring',
+      'round:competition'
+    ].each do |missing_param|
+      describe "with missing '#{missing_param} param" do
+        let(:params) { { round: { date: '2015-04-01', course_id: 1, holes_played: 9, scoring: 'stableford', competition: false } } }
+
+        before do
+          FactoryGirl.create(:course)
+          params.delete_param(missing_param)
+          post '/rounds', params
+        end
+
+        it 'responds with HTTP 400' do
+          last_response.must_be :bad_request?
+        end
+      end
+    end
+
+    [
+      # Prolly due to a bug in Grape
+      #['round', 'string'],
+      ['round:date', 42],
+      ['round:course_id', 'string'],
+      ['round:holes_played', 'string'],
+      ['round:scoring', 42],
+      ['round:competition', 'string']
+    ].each do |param, invalid_value|
+      describe "with an invalid '#{param}' param" do
+        let(:params) { { round: { date: '2015-04-01', course_id: 1, holes_played: 9, scoring: 'stableford', competition: false } } }
+
+        before do
+          FactoryGirl.create(:course)
+          params.change_param(param, invalid_value)
+          post '/rounds', params
+        end
+
+        it 'responds with HTTP 400' do
+          last_response.must_be :bad_request?
+        end
+      end
     end
   end
 end
--- spec/api/spec_helper.rb
+++ spec/api/spec_helper.rb
@@ -14,3 +14,26 @@ module Minitest
     end
   end
 end
+
+class Hash
+  def delete_param(param)
+    key, sep, left = param.partition(':')
+    if left.empty?
+      delete(key.to_sym)
+    else
+      self[key.to_sym].delete_param(left)
+    end
+    self
+  end
+
+  def change_param(param, value)
+    key, sep, left = param.partition(':')
+    if left.empty?
+      delete(key.to_sym)
+      self[key.to_sym] = value
+    else
+      self[key.to_sym].change_param(left, value)
+    end
+    self
+  end
+end
